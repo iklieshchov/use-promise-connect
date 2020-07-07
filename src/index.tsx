3eea5a9d5154030164c0356b2330b236468532f1
Ihor Klieshchov <igorklieshchov@Ihors-MacBook-Pro.local>
2020-07-07 20:07:20 +0300

    memorize senz and cancel functions

--- src/index.tsx
+++ src/index.tsx
@@ -8,33 +8,51 @@ export const usePromiseConnect = <T extends ConnectionConfig>(config: T): Connec
 
   const memorizedConfig = useMemo(() => {
     return config;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const funcs = useMemo(() => {
+
+    const names = Object.keys(memorizedConfig) as Array<keyof T>;
+    const out: any = {};
+
+    for (let name of names) {
+
+      const key = name as string;
+      const promiseFn = memorizedConfig[name];
+
+      const send = async (...args: Parameters<typeof promiseFn>) => {
+        const requestId = Date.now();
+        dispatch({ type: 'load', key, requestId });
+        try {
+          const result = await promiseFn(...args as any);
+          dispatch({ type: 'success', key, data: result, requestId });
+        } catch (e) {
+          dispatch({ type: 'error', key, error: e, requestId });
+        }
+      };
+
+      const cancel = () => {
+        dispatch({ type: 'cancel', key, requestId: Date.now() });
+      };
+
+      out[name] = { send, cancel };
+    }
+    return out;
+  }, [memorizedConfig]);
+
   return useMemo<ConnectedPromises<T>>(() => {
+
     const names = Object.keys(memorizedConfig) as Array<keyof T>;
     const out = {} as ConnectedPromises<T>;
-    for(let name of names) {
-      const promiseFn = memorizedConfig[name];
+
+    for (let name of names) {
       const key = name as string;
-      const state = conState[key];
       out[name] = {
-        send: async (...args: Parameters<typeof promiseFn>) => {
-          const requestId = Date.now();
-          dispatch({ type: 'load', key, requestId });
-          try {
-            const result = await promiseFn(...args as any);
-            dispatch({ type: 'success', key, data: result, requestId });
-          } catch(e) {
-            dispatch({ type: 'error', key, error: e, requestId });
-          }
-        },
-        cancel: () => {
-          dispatch({ type: 'cancel', key, requestId: Date.now() });
-        },
-        ...state,
+        ...funcs[name],
+        ...conState[key],
       };
     }
     return out;
-  }, [conState, memorizedConfig]);
+  }, [conState, funcs, memorizedConfig]);
 };
